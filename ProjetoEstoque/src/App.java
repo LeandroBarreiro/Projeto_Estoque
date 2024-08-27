3bc96873f96b06f40259a974dc373f312b5cea7a
LeandroBarreiro <leandrobarreiro15@gmail.com>
2024-08-27 18:44:14 -0400

    modified:   ProjetoEstoque/src/App.java

--- ProjetoEstoque/src/App.java
+++ ProjetoEstoque/src/App.java
@@ -4,6 +4,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         ClasseProdutoVisao visao = new ClasseProdutoVisao();
         visao.Exibir();
-        System.out.println("Hello World");
     }
 }
